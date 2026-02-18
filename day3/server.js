7326d3b2f17179487658ba1864716a91ca0dd7c6
Akram khan <khanakram40507@gmail.com>
2026-02-18 21:58:41 +0530

    notes mai jahkar dekhna

--- day3/server.js
+++ day3/server.js
@@ -2,6 +2,7 @@ const express= require('express');
 const app= express();
 
 app.use(express.json());
+const notes = [];
 
 app.get('/',(req,res)=>{
     res.send('Hello World from Express!');
@@ -9,9 +10,13 @@ app.get('/',(req,res)=>{
 
 app.post('/note',(req,res)=>{
     console.log(req.body);
+    notes.push(req.body);
     res.send('Note created successfully!');
 
 } );
+app.get('/note',(req,res)=>{
+    res.send(notes);
+});
 
 app.listen(3000,()=>{
     console.log('Server is running on port 3000');
